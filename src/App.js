975cf41c731648e22c9d901562174e2926fab9df
Bogdan Baghiu <bogdantrabajo@gmail.com>
2022-08-24 22:37:08 +0200

    Add biblical flood annotations

--- src/App.js
+++ src/App.js
@@ -19,6 +19,41 @@ class App extends Component {
         },
       ],
       options: {
+        annotations: {
+          xaxis: [
+            {
+              x: 1656,
+              type: 'numeric',
+              borderColor: "transparent",
+              label: {
+                borderColor: "transparent",
+                style: {
+                  color: "#FEB019",
+                  fontSize: '14px',
+                },
+                orientation: "horizontal",
+                text: "1656",
+                position: "bottom",
+                offsetY: 22,
+              },
+            },
+            {
+              x: 1656,
+              type: 'numeric',
+              borderColor: "#FEB019",
+              label: {
+                borderColor: "#FEB019",
+                style: {
+                  color: "#fff",
+                  background: "#FEB019",
+                  fontSize: '14px',
+                },
+                orientation: "horizontal",
+                text: "POTOP",
+              }
+            },
+          ]
+        },
         colors: ['#ff0000', '#E7E6E6'],
         chart: {
           type: 'rangeBar',

80bac1a14cd06cc58e65585ae496926361c0aa27
Bogdan Baghiu <bogdantrabajo@gmail.com>
2022-08-24 19:03:01 +0200

    Add timeline chart from patriarch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+import BORNDATA from "./data/born";
+import LIVEDATA from "./data/live";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [
+        {
+          name: 'Perioadă de timp fără fii',
+          data: BORNDATA
+        },
+        {
+          name: 'Perioadă de timp cu fii',
+          data: LIVEDATA
+        },
+      ],
+      options: {
+        colors: ['#ff0000', '#E7E6E6'],
+        chart: {
+          type: 'rangeBar',
+          width: '80%',
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            barHeight: '80%',
+            rangeBarGroupRows: true,
+            dataLabels: {
+              maxItems: 100,
+              hideOverflowingLabels: false,
+            }
+          }
+        },
+        xaxis: {
+          type: 'numeric'
+        },
+        stroke: {
+          width: 0
+        },
+        fill: {
+          type: 'solid',
+          opacity: 1
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'left'
+        },
+        dataLabels: {
+          enabled: true,
+          style: {
+            fontSize: '14px',
+            colors: ['#fff', '#1B1A17']
+          }
+        },
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="mixed-chart">
+          <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type="rangeBar"
+            width="110%"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
